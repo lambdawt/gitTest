1476e145a250276e4e2b23535a21fdc4db781a4f
zwt <1240355745@qq.com>
2021-06-06 22:39:07 +0800

    创建、编辑HelloWorld类

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: HelloWorld
+ * @Description: HelloWorld类，练习idea中使用git
+ * @Author: ZWT
+ * @Date: 2021/6/6 22:36
+ * @Version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
